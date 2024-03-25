cd4bc05d37aca96a61efc919a60e86d15d03afd1
harrisoncalmeida <harrisoncalmeida@gmail.com>
2024-03-25 19:09:39 -0300

    Novas alterações

--- PYTHON/ESTUDOS/estudo12.py
+++ PYTHON/ESTUDOS/estudo12.py
@@ -78,3 +78,134 @@ imc = []
 for i in range(len(p)):
   imc.append(round(p[i]/a[i]**2,2))
 print(f'A pessoa tem IMC de: {imc}')
+
+'''
+Módulos são scripts que podem ser salvos com a extensão .py e importados
+por outros arquivos python
+
+vamos abrir um novo colab e criar um módulo
+'''
+
+# Módulo Numpy
+import numpy as np
+
+# Módulo Pandas
+import pandas as pd
+
+# Módulo Seaborn
+import seaborn as sns
+
+# Módulo Matplotlib
+import matplotlib as plt
+
+pip install pymysql
+
+import pymysql
+
+'''
+Elabore um programa que receba do usuário em um único input
+uma sequência de números e calcule e exiba:
+a) a média aritmética
+b) a variância
+c) o desvio padrão
+'''
+dados = input('Informe os valores numéricos separados por espaços: ') # recebe uma string com todos os valores desejados
+lista = dados.split() # converte a string em uma lista contendo strings numérica separadas
+n = len(lista)
+for i in range(n):
+  lista[i] = float(lista[i])
+media = sum(lista)/n
+quad = []
+for i in range(n):
+  quad.append((lista[i] - media)**2)
+var = sum(quad)/(n-1)
+desvio = var**(1/2)
+print(f'A média é {media:.2f}')
+print(f'A variância é {var:.2f}')
+print(f'O desvio padrão é {desvio:.2f}')
+
+'''
+Elabore um programa que receba do usuário em um único input
+uma sequência de números e calcule e exiba:
+a) a média aritmética
+b) a variância
+c) o desvio padrão
+'''
+dados = input('Informe os valores numéricos separados por espaços: ') # recebe uma string com todos os valores desejados
+lista = dados.split() # converte a string em uma lista contendo strings numérica separadas
+n = len(lista)
+for i,v in enumerate(lista):
+  lista[i] = float(lista[i])
+media = sum(lista)/n
+quad = []
+for i,v in enumerate(lista):
+  quad.append((lista[i] - media)**2)
+var = sum(quad)/(n-1)
+desvio = var**(1/2)
+print(f'A média é {media:.2f}')
+print(f'A variância é {var:.2f}')
+print(f'O desvio padrão é {desvio:.2f}')
+
+# alternativa usando função com args
+def media(*args):
+  soma = 0
+  quad = []
+  n = len(args)
+  for arg in args:
+    soma += arg
+  media = soma / n
+  for arg in args:
+    quad.append((arg-media)**2)
+  var = sum(quad)/(n-1)
+  desvio = var**(1/2)
+  return media, var, desvio
+
+# Como ordenar de forma crescente os valores de uma lista:
+
+lista = [10, 5, 6, 9, 8, 7]
+lista = sorted(lista)
+print(lista)
+
+# para n ímpar
+nomes = ['Douglas', 'Daniela', 'Maria', 'Pedro', 'Eduardo']
+n = len(nomes)
+print(n)
+print(f'O nome que separa a lista em duas partes é {nomes[(int((n+1)/2))-1]}')
+
+# para n par
+nomes2 = ['Douglas', 'Daniela', 'Maria', 'Pedro', 'Eduardo','Ester']
+n = len(nomes2)
+print(n)
+print(f'Os nomes centrais {nomes2[int(n/2)-1]} e {nomes2[int(n/2)]}')
+
+'''
+Implente a seguinte funcionalidade no programa:
+cálculo e exibição da mediana
+'''
+'''
+Elabore um programa que receba do usuário em um único input
+uma sequência de números e calcule e exiba:
+a) a média aritmética
+b) a variância
+c) o desvio padrão
+'''
+dados = input('Informe os valores numéricos separados por espaços: ') # recebe uma string com todos os valores desejados
+lista = dados.split() # converte a string em uma lista contendo strings numérica separadas
+n = len(lista)
+for i in range(n):
+  lista[i] = float(lista[i])
+media = sum(lista)/n
+quad = []
+for i in range(n):
+  quad.append((lista[i] - media)**2)
+var = sum(quad)/(n-1)
+desvio = var**(1/2)
+lista = sorted(lista)
+if n % 2 == 0:
+  mediana = (lista[int(n/2)-1] + lista[int(n/2)])/2
+else:
+  mediana = lista[int((n+1)/2)-1]
+print(f'A média é {media:.2f}')
+print(f'A variância é {var:.2f}')
+print(f'O desvio padrão é {desvio:.2f}')
+print(f'O Valor da mediana é {mediana:.2f}')
